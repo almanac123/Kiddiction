0c6f1024ad7c7d2848a51cbf1b9b88558e1b64f5
Anubhab <anubhabpatra02@gmail.com>
2022-12-25 13:08:28 +0530

    learn section added

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Learn from "./components/Learn/Learn";
+
 import Navbar from "./components/Navbar";
+
 function App() {
     return(
-        <div>
+        <>
+        {/* <div>
         <Navbar/>
+        </div> */}
+        <div>
+        <Learn/>
         </div>
+        </>
     )
  
 }
--- project/src/components/Navbar.js
+++ project/src/components/Navbar.js
@@ -12,21 +12,21 @@ const Navbar = () => {
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4  '>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
       <ul className='hidden md:flex cursor-pointer hover:font-normal'>
-        <li className='p-4' href='/'>Home</li>
-        <li className='p-4' href='/about'>About</li>
-        <li className='p-4' href='/contact'>Contact</li>
-        <li className='p-4' href='/login'>Login</li>
-        <li className='p-4' href='/register'>Register</li>
+        <li className='p-4 text-[#00df9a] text-lg font-bold' href='/'>Home</li>
+        <li className='p-4 text-[#00df9a] text-lg font-bold' href='/contact'>Learn</li>
+        <li className='p-4 text-[#00df9a] text-lg font-bold' href='/about'>Games</li>
+        <li className='p-4 text-[#00df9a] text-lg font-bold' href='/login'>Parents</li>
+        <li className='p-4 text-[#00df9a] text-lg font-bold' href='/register'>Register</li>
       </ul>
       <div onClick={handleNav} className='block md:hidden cursor-pointer'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
       <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%] '}>
         <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
-          <li className='p-4 border-b border-gray-600 text-white' href='/'>Home</li>
-          <li className='p-4 border-b border-gray-600 text-white' href='/about'>About</li>
-          <li className='p-4 border-b border-gray-600 text-white' href='/contact'>Contact</li>
-          <li className='p-4 border-b border-gray-600 text-white' href='/login'>Login</li>
+          <li className='p-4 border-b border-gray-600 text-[#00df9a]' href='/'>Home</li>
+          <li className='p-4 border-b border-gray-600 text-[#00df9a]' href='/about'>Learn</li>
+          <li className='p-4 border-b border-gray-600 text-[#00df9a]' href='/contact'>Games</li>
+          <li className='p-4 border-b border-gray-600 text-[#00df9a]' href='/login'>Parents</li>
           <li className='p-4 text-white' href='/register'>Register</li>
       </ul>
     </div>
